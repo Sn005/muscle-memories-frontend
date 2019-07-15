6e2847130f563d7264d6ace379a0b869bc8078fb
snasna <wisperinthedustbox@gmail.com>
2019-07-15 21:15:36 +0900

    container/desktop/AppNavigationの仮実装

---
+++ src/components/containers/desktop/AppNavigation.vue
@@ -0,0 +1,53 @@
+<template>
+  <div @mouseover.stop="openNavigation" @mouseleave.stop="closeNavigation">
+    <v-navigation-drawer drawer fixed app :miniVariant="miniVariant">
+      <!-- <div @click="mouseover">hoge</div> -->
+      <v-list>
+        <v-list-tile
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title" />
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      miniVariant: true,
+      items: [
+        {
+          icon: 'apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'bubble_chart',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      title: 'Vuetify.js'
+    }
+  },
+  methods: {
+    openNavigation() {
+      this.miniVariant = false
+    },
+    closeNavigation() {
+      this.miniVariant = true
+    }
+  }
+}
+</script>
