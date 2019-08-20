b339c568861e4b07f85bb5551824e5fe1ec09892
snasna <wisperinthedustbox@gmail.com>
2019-08-21 00:23:21 +0900

    calenderComponentの仮組

--- src/api/http/mocks/entities/TrainingRecords.ts
+++ src/api/http/mocks/entities/TrainingRecords.ts
@@ -7,11 +7,14 @@ import {
 } from '@/interfaces/api/http/ITrainingRecords'
 const targets = ['胸', '腕', '肩', '背中', '脚']
 
-export const trainingRecords = [...Array(40)].map(
+export const trainingRecords = [...Array(30)].map(
   (v, i): ITrainingRecordsModel => {
     console.log('hoge')
     const title = targets[Math.floor(Math.random() * targets.length)] + 'の日'
-    const trainingDate = faker.date.between(moment().add(-3, 'M'), moment())
+    const trainingDate = faker.date.between(
+      moment().add(1, 'M'),
+      moment().add(-1, 'M')
+    )
     const sets: ITrainingRecordsSets[] = [...Array(3)].map(v => {
       return {
         weight: faker.random.number({ min: 80, max: 120 }),
--- src/components/organisms/TrainingRecordsCalender/desktop.vue
+++ src/components/organisms/TrainingRecordsCalender/desktop.vue
@@ -1,16 +1,44 @@
 <template>
-  <div>
-    hoge
-  </div>
+  <v-sheet height="500">
+    <v-calendar
+      type="month"
+      :now="now"
+      :events="formatedTrainingRecords"
+    ></v-calendar>
+  </v-sheet>
 </template>
 <script lang="ts">
+import moment from 'moment'
 import Vue, { PropType } from 'vue'
 import { ITrainingRecordsModel } from '@/interfaces/api/http/ITrainingRecords'
 
+interface IData {
+  now: string
+}
+interface IFormatedTrainingRecords {
+  name: string
+  start: string
+}
 export default Vue.extend({
   name: 'DesktopTrainingRecordsCalender',
   props: {
     trainingRecords: Array as PropType<ITrainingRecordsModel[] | null>
+  },
+  data(): IData {
+    return {
+      now: moment().format('YYYY-MM-DD')
+    }
+  },
+  computed: {
+    formatedTrainingRecords(): IFormatedTrainingRecords[] {
+      if (!this.trainingRecords) return []
+      return this.trainingRecords.map(v => {
+        return {
+          name: v.title,
+          start: moment(v.trainingDate).format('YYYY-MM-DD')
+        }
+      })
+    }
   }
 })
 </script>
