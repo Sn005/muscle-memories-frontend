7a9ed3eda58a4cd4e51a9336aae9840b912db6ff
snasna <wisperinthedustbox@gmail.com>
2019-08-20 18:22:02 +0900

    pages/index.tsならびに-desktop.vue作成

---
+++ src/pages/-desktop.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-layout wrap>
+    <v-card>
+      <v-card-title class="headline"
+        >Welcome to the Vuetify + Nuxt.js template</v-card-title
+      >
+      <v-card-text>
+        hoge
+      </v-card-text>
+    </v-card>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  head() {
+    return {
+      title: 'dddfdf'
+    }
+  }
+})
+</script>
